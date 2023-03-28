4aedfb905d088d1f2d71a4b7090bce6810cf5760
Артемий <artema121rus@mail.ru>
2023-03-28 10:52:37 +0300

    Добавляет кнопку загрузить ещё

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,3 +1,5 @@
+const COMMENTS_PORTION = 5;
+
 const body = document.querySelector('body');
 const bigPhoto = document.querySelector('.big-picture');
 const commentCount = document.querySelector('.social__comment-count');
@@ -5,6 +7,9 @@ const commentsList = document.querySelector('.social__comments');
 const commentTemplate = document.querySelector('.social__comment');
 const commentsLoader = document.querySelector('.comments-loader');
 const cancelButton = document.querySelector('.big-picture__cancel');
+
+let comments = [];
+let commentsShown = 0;
 
 const createComment = (data) => {
   const comment = commentTemplate.cloneNode(true);
@@ -16,21 +21,32 @@ const createComment = (data) => {
   return (comment);
 };
 
-const createComments = (comments) => {
-  commentsList.textContent = '';
-  const commentFragment = document.createDocumentFragment();
+const createComments = () => {
+  commentsShown += COMMENTS_PORTION;
 
-  for (let i = 0; i < comments.length; i++) {
+  if (commentsShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const commentFragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
     const commentElement = createComment(comments[i]);
     commentFragment.append(commentElement);
   }
+
+  commentsList.innerHTML = '';
   commentsList.append(commentFragment);
+  commentCount.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> комментариев`;
 };
 
 const closeBigPhoto = () => {
   bigPhoto.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  commentsShown = 0;
 };
 
 function onDocumentKeydown (evt) {
@@ -44,6 +60,10 @@ const onCancelButtonClick = () => {
   closeBigPhoto();
 };
 
+const onCommentsLoaderClick = () => {
+  createComments();
+};
+
 const createBigPhoto = ({url, likes, description}) => {
   bigPhoto.querySelector('.big-picture__img img').src = url;
   bigPhoto.querySelector('.big-picture__img img').alt = description;
@@ -54,14 +74,16 @@ const createBigPhoto = ({url, likes, description}) => {
 const showBigPhoto = (data) => {
   bigPhoto.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentsLoader.classList.add('hidden');
-  commentCount.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
   createBigPhoto(data);
-  createComments(data.comments);
+  comments = data.comments;
+  if (comments.length > 0) {
+    createComments(comments);
+  }
 };
 
 cancelButton.addEventListener('click', onCancelButtonClick);
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
 
 export {showBigPhoto};
