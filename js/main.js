012e83b5b8888de6c4f0ce96e424b3b927b13b04
Артемий <artema121rus@mail.ru>
2023-04-08 08:50:37 +0300

    Добавялет исправления надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import {createGallery} from './gallery.js';
 import {getData, sendData} from './api.js';
-import {showAlert} from './util.js';
+import {showAlert} from './alert.js';
 import {setOnFormSubmit, closeModal} from './form.js';
-import {showOkMessage, showErrMessage} from './message.js';
+import {showSuccessMessage, showErrorMessage} from './message.js';
 
 setOnFormSubmit(async (data) => {
   try {
     await sendData(data);
     closeModal();
-    showOkMessage();
+    showSuccessMessage();
   } catch {
-    showErrMessage();
+    showErrorMessage();
   }
 });
 
--- js/message.js
+++ js/message.js
@@ -1,78 +1,26 @@
-const submitMessagePlaceErr = document.querySelector('body');
-const submitErrMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
 
-const messageErrElement = submitErrMessageTemplate.cloneNode(true);
-submitMessagePlaceErr.append(messageErrElement);
-messageErrElement.classList.add('hidden');
-
-const submitErrMessageContainer = document.querySelector('.error');
-const closeModalSubmitErr = document.querySelector('.error__button');
-const submitOkMessageTemplate = document.querySelector('#success').content.querySelector('.success');
 const body = document.querySelector('body');
 
-const closeErrMessage = () => {
-  submitErrMessageContainer.classList.add('hidden');
-  body.classList.remove('modal-open');
+const showSuccessMessage = () => {
+  const templateSuccess = document.querySelector('#success').content;
+  const sectionSuccess = templateSuccess.querySelector('section');
+  const cloneSectionSuccess = sectionSuccess.cloneNode(true);
+  body.appendChild(cloneSectionSuccess);
 };
 
-const onModalKeydownErr = (evt) => {
-  if (evt.key === 'Escape'(evt)) {
-    evt.preventDefault();
-    closeErrMessage();
-  }
+
+const showErrorMessage = () => {
+  const templateError = document.querySelector('#error').content;
+  const sectionError = templateError.querySelector('section');
+  const cloneSectionError = sectionError.cloneNode(true);
+  body.appendChild(cloneSectionError);
 };
 
-document.addEventListener('click', (evt) => {
-  if (evt.target.classList.contains('error')) {
-    closeErrMessage();
-  }
-});
-
-const showErrMessage = () => {
-  submitErrMessageContainer.classList.remove('hidden');
-  body.classList.add('modal-open');
-
-  document.addEventListener('keydown', onModalKeydownErr);
-};
-
-closeModalSubmitErr.addEventListener('click', () => {
-  closeErrMessage();
-});
-
-const messageOkElement = submitOkMessageTemplate.cloneNode(true);
-body.append(messageOkElement);
-messageOkElement.classList.add('hidden');
-
-const submitOkMessageContainer = document.querySelector('.success');
-const closeModalSubmitOk = document.querySelector('.success__button');
-
-const closeOkMessage = () => {
-  submitOkMessageContainer.classList.add('hidden');
-  body.classList.remove('modal-open');
-};
-
-const onModalKeydownOk = (evt) => {
-  if (evt.key === 'Escape'(evt)) {
-    evt.preventDefault();
-    closeOkMessage();
-  }
-};
-
-document.addEventListener('click', (evt) => {
-  if (evt.target.classList.contains('success')) {
-    closeOkMessage();
-  }
-});
-
-const showOkMessage = () => {
-  submitOkMessageContainer.classList.remove('hidden');
-  body.classList.add('modal-open');
-
-  document.addEventListener('keydown', onModalKeydownOk);
-};
-
-closeModalSubmitOk.addEventListener('click', () => {
-  closeOkMessage();
-});
-
-export {showOkMessage, showErrMessage};
+export { debounce, showSuccessMessage, showErrorMessage };
