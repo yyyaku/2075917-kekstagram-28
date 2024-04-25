ba30ecda0c2031defdf4e009d0e5eba073325032
Артемий <artema121rus@mail.ru>
2024-04-25 11:00:36 +0300

    fix: правит ссылку на данные

--- js/api.js
+++ js/api.js
@@ -1,22 +1,21 @@
-const BASE_URL = 'https://28.javascript.pages.academy/kekstagram';
+const BASE_URL = "https://28.javascript.htmlacademy.pro/kekstagram";
 const Route = {
-  GET_DATA: '/data',
-  SEND_DATA: '/',
+  GET_DATA: "/data",
+  SEND_DATA: "/",
 };
 
 const Method = {
-  GET: 'GET',
-  POST: 'POST',
+  GET: "GET",
+  POST: "POST",
 };
 
 const ErrorText = {
-  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
-  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+  GET_DATA: "Не удалось загрузить данные. Попробуйте обновить страницу",
+  SEND_DATA: "Не удалось отправить форму. Попробуйте ещё раз",
 };
 
 const load = (route, errorText, method = Method.GET, body = null) =>
-  fetch(
-    `${BASE_URL}${route}`, {method, body})
+  fetch(`${BASE_URL}${route}`, { method, body })
     .then((response) => {
       if (!response.ok) {
         throw new Error();
@@ -29,6 +28,7 @@ const load = (route, errorText, method = Method.GET, body = null) =>
 
 const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
 
-const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+const sendData = (body) =>
+  load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
 
-export {getData, sendData};
+export { getData, sendData };
